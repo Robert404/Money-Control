cfb4f0bb73f9df15a80385ebdabdb072230a2d25
Robert <robners404@gmail.com>
2021-06-28 19:49:04 +0400

    Initial Commit

---
+++ Money Control/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Money Control
+//
+//  Created by Robert Nersesyan on 28.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

d4816d47dfaf874cb9bfb987631736b11b212778
Robert <robners404@gmail.com>
2021-08-25 23:08:00 +0400

    Some changes to alerts and files

--- Money Control/ViewControllers/AddTransactionViewController.swift
+++ Money Control/ViewControllers/AddTransactionViewController.swift
@@ -18,24 +18,23 @@ class AddTransactionViewController: UIViewController {
     var isExpense: Bool = true
     override func viewDidLoad() {
         super.viewDidLoad()
-        name?.becomeFirstResponder()
+        name.becomeFirstResponder()
         name.borderStyle = UITextField.BorderStyle.roundedRect
         amount.borderStyle = UITextField.BorderStyle.roundedRect
         date.borderStyle = UITextField.BorderStyle.roundedRect
         saveBtn.layer.cornerRadius = 15
     }
     
-    //TODO: Better alerts here
     @IBAction func saveTransaction() {
         let amountDouble = Double(amount.text!)
-        if !name.text!.isEmpty && amountDouble != 0 && isDateVaid(for: date.text!) {
+        if !name.text!.isEmpty && amountDouble != 0 && !amount.text!.isEmpty
+            && isDateValid(for: date.text!) {
             let transaction = Transaction(sum: amountDouble ?? 0, name: name.text!, date: date.text!, type: "test", isExpense: self.isExpense)
             completion?(transaction)
             
-        } else {
-            let alert = UIAlertController(title: "Warning!", message: "Please Enter required fields", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Gotcha", style: .default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
+        }
+        else {
+            chooseAlertWindow()
         }
     }
     
@@ -48,12 +47,29 @@ class AddTransactionViewController: UIViewController {
         }
     }
     
-    func isDateVaid(for string: String) -> Bool {
+    func isDateValid(for string: String) -> Bool {
         let pattern = "[0-3][0-9]/[0-1][0-9]/[0-9]{4}"
         let regex = try! NSRegularExpression(pattern: pattern, options: .caseInsensitive)
         
         let matches = regex.matches(in: string, options: .reportProgress, range:NSRange(location: 0, length: string.count))
         
         return matches.count > 0 ? true: false
+    }
+    
+    func chooseAlertWindow() {
+        var titleOfAlert: String
+        var messageOfAlert: String
+        
+        if !isDateValid(for: date.text!) {
+            titleOfAlert = "Yoooo"
+            messageOfAlert = "You need to enter valid date format"
+        }
+        else {
+            titleOfAlert = "Bruh"
+            messageOfAlert = "Fill every value in textfields"
+        }
+        let alert = UIAlertController(title: titleOfAlert, message: messageOfAlert, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Gotcha", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
 }
--- Money Control/ViewControllers/ViewController.swift
+++ Money Control/ViewControllers/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController {
 
     @IBOutlet var totalSumLabel: UILabel!
     @IBOutlet var incomeLabel: UILabel!
@@ -43,87 +43,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         navigationController?.pushViewController(vc, animated: true)
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return transactions.count
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 60
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        table.deselectRow(at: indexPath, animated: true)
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TransactionTableViewCell
-        cell.name.text = transactions[indexPath.row].name
-        cell.sum.text = "$" + String(transactions[indexPath.row].sum)
-        cell.date.text = String(transactions[indexPath.row].date)
-        return cell
-    }
-    
-    //TODO: Cleanup this method
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            let totalMoney = totalSumLabel.text?.replacingOccurrences(of: "$", with: "")
-            let incomeMoney = incomeLabel.text?.replacingOccurrences(of: "$", with: "")
-            let expenseMoney = expenseLabel.text?.replacingOccurrences(of: "$", with: "")
-            
-            let deletedTransaction = transactions.remove(at: indexPath.row)
-            switch deletedTransaction.isExpense {
-            case true:
-                let updatedTotal = Double(totalMoney!)! + deletedTransaction.sum
-                let updatedExpence = Double(expenseMoney!)! - deletedTransaction.sum
-                expenseLabel.text! = "$" + String(updatedExpence)
-                totalSumLabel.text! = "$" + String(updatedTotal)
-            case false:
-                let updatedTotal = Double(totalMoney!)! - deletedTransaction.sum
-                let updatedIncome = Double(incomeMoney!)! - deletedTransaction.sum
-                incomeLabel.text! = "$" + String(updatedIncome)
-                totalSumLabel.text! = "$" + String(updatedTotal)
-            }
-            table.reloadData()
-            saveItems()
-        }
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        switch segue.identifier {
-            
-        case "transactionScreen":
-            if let indexPath = table.indexPathForSelectedRow {
-                let destinationController = segue.destination as! TransactionViewController
-                destinationController.transaction = transactions[indexPath.row]
-            }
-        default:
-            break
-        }
-    }
-    
-    //TODO: cleanup this method
-    private func calculateTotalMoney(transactionSum: Double, isExpence: Bool) {
-        var updatedTotal = 0.0
-        var income = 0.0
-        var expense = 0.0
-
-        let totalMoney = totalSumLabel.text?.replacingOccurrences(of: "$", with: "")
-        let incomeMoney = incomeLabel.text?.replacingOccurrences(of: "$", with: "")
-        let expenseMoney = expenseLabel.text?.replacingOccurrences(of: "$", with: "")
-
-        if isExpence {
-            updatedTotal = Double(totalMoney!)! - transactionSum
-            expense = Double(expenseMoney!)! + transactionSum
-            expenseLabel.text = "$" + String(expense)
-        } else {
-            updatedTotal = Double(totalMoney!)! + transactionSum
-            income = Double(incomeMoney!)! + transactionSum
-            incomeLabel.text = "$" + String(income)
-        }
-        totalSumLabel.text = "$" + String(updatedTotal)
-    }
-    
-    //TODO: cleanup
     private func getItems() {
         guard
             let data = UserDefaults.standard.data(forKey: "items"),
@@ -152,6 +71,87 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     private func saveItems() {
         if let encodedData = try? JSONEncoder().encode(transactions) {
             UserDefaults.standard.set(encodedData, forKey: "items")
+        }
+    }
+    
+    private func calculateTotalMoney(transactionSum: Double, isExpence: Bool) {
+        var updatedTotal = 0.0
+        var income = 0.0
+        var expense = 0.0
+
+        let totalMoney = totalSumLabel.text?.replacingOccurrences(of: "$", with: "")
+        let incomeMoney = incomeLabel.text?.replacingOccurrences(of: "$", with: "")
+        let expenseMoney = expenseLabel.text?.replacingOccurrences(of: "$", with: "")
+
+        if isExpence {
+            updatedTotal = Double(totalMoney!)! - transactionSum
+            expense = Double(expenseMoney!)! + transactionSum
+            expenseLabel.text = "$" + String(expense)
+        } else {
+            updatedTotal = Double(totalMoney!)! + transactionSum
+            income = Double(incomeMoney!)! + transactionSum
+            incomeLabel.text = "$" + String(income)
+        }
+        totalSumLabel.text = "$" + String(updatedTotal)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+            
+        case "transactionScreen":
+            if let indexPath = table.indexPathForSelectedRow {
+                let destinationController = segue.destination as! TransactionViewController
+                destinationController.transaction = transactions[indexPath.row]
+            }
+        default:
+            break
+        }
+    }
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return transactions.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        table.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TransactionTableViewCell
+        cell.name.text = transactions[indexPath.row].name
+        cell.sum.text = "$" + String(transactions[indexPath.row].sum)
+        cell.date.text = String(transactions[indexPath.row].date)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let totalMoney = totalSumLabel.text?.replacingOccurrences(of: "$", with: "")
+            let incomeMoney = incomeLabel.text?.replacingOccurrences(of: "$", with: "")
+            let expenseMoney = expenseLabel.text?.replacingOccurrences(of: "$", with: "")
+            
+            let deletedTransaction = transactions.remove(at: indexPath.row)
+            switch deletedTransaction.isExpense {
+            case true:
+                let updatedTotal = Double(totalMoney!)! + deletedTransaction.sum
+                let updatedExpence = Double(expenseMoney!)! - deletedTransaction.sum
+                expenseLabel.text! = "$" + String(updatedExpence)
+                totalSumLabel.text! = "$" + String(updatedTotal)
+            case false:
+                let updatedTotal = Double(totalMoney!)! - deletedTransaction.sum
+                let updatedIncome = Double(incomeMoney!)! - deletedTransaction.sum
+                incomeLabel.text! = "$" + String(updatedIncome)
+                totalSumLabel.text! = "$" + String(updatedTotal)
+            }
+            table.reloadData()
+            saveItems()
         }
     }
 }
